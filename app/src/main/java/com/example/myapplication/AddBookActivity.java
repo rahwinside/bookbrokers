1532cfbf3029218b8898920419dab8a2bd6e233c
[Rahul Pon] <[theflyingrahul@gmail.com]>
2020-02-25 17:45:42 +0530

    added profile int ~!

--- app/src/main/java/com/example/myapplication/AddBookActivity.java
+++ app/src/main/java/com/example/myapplication/AddBookActivity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -24,12 +26,14 @@ import java.util.Map;
 public class AddBookActivity extends AppCompatActivity {
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth;
+
     CollectionReference collectionReference;
     EditText bName, author, price;
 
     String dept;
     int sem;
-    String subject;
+    String subject, uid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,33 +43,66 @@ public class AddBookActivity extends AppCompatActivity {
         author = findViewById(R.id.editText3);
         price = findViewById(R.id.editText4);
 
+        mAuth = FirebaseAuth.getInstance();
+        uid = mAuth.getUid();
+
         sem = Integer.parseInt(getIntent().getStringExtra("SEM"));
         dept = getIntent().getStringExtra("DEPT");
         subject = getIntent().getStringExtra("SUBJECT");
         collectionReference = db.collection("books").document(dept.toLowerCase()).collection(Integer.toString(sem)).document(subject).collection("collection");
 
+
     }
 
     public void addBook(View view) {
+
+
         Map<String, Object> bookInfo = new HashMap<>();
         bookInfo.put("name", bName.getText().toString());
         bookInfo.put("author", author.getText().toString());
         bookInfo.put("price", price.getText().toString());
-        bookInfo.put("user", "admin");
-        collectionReference.document().set(bookInfo).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                Snackbar.make(view, "Book has been posted successfully!", Snackbar.LENGTH_LONG)
+        bookInfo.put("user", uid);
+        collectionReference.add(bookInfo)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+
+                        String docID = documentReference.getId();
+                        db.collection("user").document(uid).collection("myads").document(docID).set(bookInfo).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Snackbar.make(view, "Book has been posted successfully!", Snackbar.LENGTH_LONG)
+                                        .setAction("Dismiss", null).show();
+                                finish();
+                            }
+                        });
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Snackbar.make(view, "Error posting book, please try again later.", Snackbar.LENGTH_LONG)
                         .setAction("Dismiss", null).show();
-                finish();
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Snackbar.make(view, "Error posting book, please try again later.", Snackbar.LENGTH_LONG)
-                        .setAction("Dismiss", null).show();
-            }
-        });
+                    }
+                });
+
+//                .document().set(bookInfo).addOnCompleteListener(new OnCompleteListener<Void>() {
+//            @Override
+//            public void onComplete(@NonNull Task<Void> task) {
+//                Snackbar.make(view, "Book has been posted successfully!", Snackbar.LENGTH_LONG)
+//                        .setAction("Dismiss", null).show();
+//                finish();
+//            }
+//        }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Snackbar.make(view, "Error posting book, please try again later.", Snackbar.LENGTH_LONG)
+//                        .setAction("Dismiss", null).show();
+//            }
+//        });
+
+
 
 
     }
--- app/src/main/java/com/example/myapplication/ProfileFragment.java
+++ app/src/main/java/com/example/myapplication/ProfileFragment.java
@@ -1,19 +1,59 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class ProfileFragment extends Fragment {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth mAuth;
+    TextView name;
+    CollectionReference collectionReference;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile,container,false);
+        View v = inflater.inflate(R.layout.fragment_profile,container,false);
+        name = v.findViewById(R.id.name);
 
+        mAuth = FirebaseAuth.getInstance();
+        String uid = mAuth.getUid();
+
+        db.collection("user").document(uid)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                name.setText("Hi "+document.get("name").toString()+"!");
+                            } else {
+                            }
+                        } else {
+                            System.out.println("Get failed with "+task.getException());
+                        }
+                    }
+                });
+
+
+        return  v;
     }
 }
